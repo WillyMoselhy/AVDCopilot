12e2b7d70ee1c1daa65079fc3f1d3578fd1999dd
Willy Moselhy [MSFT] <willy@moselhy.com>
2025-05-15 14:12:25 +0000

    Add Portal UI

---
+++ helper/helper.ps1
@@ -0,0 +1,4 @@
+# These are used to help with development process. Does not have any impact on the actual code.
+
+
+bicep build  .\src\Azure\bicep\AVDCopilotLogicApps.bicep --outfile .\src\Azure\arm\AVDCopilotLogicApps.json
